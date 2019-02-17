06e327841073040fbd7e35b9da0cbda8b76e4b29
Brian Lai <brianlai30@outlook.com>
2019-02-17 01:28:20 -0800

    Added copyBuffer function

--- filecopy.c
+++ filecopy.c
@@ -14,23 +14,40 @@
 
 #define NAME "Brian Lai"
 
+int copyBuffer(int fileIn, int fileOut, void* buffer, size_t bufferSize) {
+    for (;;) {
+        void *pos;
+
+        ssize_t bytesToWrite = read(fileIn, buffer, bufferSize);
+        
+        pos = buffer;
+        while (bytesToWrite > 0) {
+            ssize_t bytesWritten = write(fileOut, pos, bytesToWrite);
+            bytesToWrite -= bytesWritten;
+            pos += bytesWritten;
+        }
+    }
+    
+    return 0;
+}
+
 int main(int argc, char const *argv[]) {
-    char inputFile[128];
-    char outputFile[128];
-    int bytesCopied;
+    int inputFile, outputFile;
+    char inputFilename[128];
+    char outputFilename[128];
+    size_t bytesCopied;
+    int bytesWritten;
 
     printf("Welcome to the File Copy Program by %s\n", NAME);
     printf("Enter the name of the file to copy from:\n");
-    scanf("%s", inputFile);
+    scanf("%s", inputFilename);
     printf("Enter the name of the file to copy to:\n");
-    scanf("%s", outputFile);
+    scanf("%s", outputFilename);
 
-    open(inputFile, 0);
-    bytesCopied = read(0, inputFile, BUFF_MAX);
-    write(1, BUFF_MAX, bytesCopied);
-    open(outputFile, 1);
+    inputFile = open(inputFilename, 0);
+    outputFile = open(outputFilename, 1);
 
-    printf("File Copy Successful, %d bytes copied", bytesCopied);
+    printf("File Copy Successful, %d bytes copied\n", bytesWritten);
 
     return 0;
 }
